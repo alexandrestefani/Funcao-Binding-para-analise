b778c0c43632704323ec476a7335f31208dd9497
Alexandre Marin Stefani <123953268+alexandrestefani@users.noreply.github.com>
2023-03-12 08:48:27 -0300

    Atualização da funcionalidade

--- app/src/main/java/com/alexandrestefani/listapp/ListAdapter.kt
+++ app/src/main/java/com/alexandrestefani/listapp/ListAdapter.kt
@@ -7,7 +7,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.alexandrestefani.listapp.databinding.LayoutBinding
 
-class ListAdapter: RecyclerView.Adapter<ListAdapter.ListViewholder>() {
+class ListAdapter(
+    val listItem: MutableList<ListItem>,
+    val listener: ListAdapterListener
+        ): RecyclerView.Adapter<ListAdapter.ListViewholder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewholder {
         return ListViewholder(LayoutBinding.inflate(
@@ -18,17 +21,29 @@ class ListAdapter: RecyclerView.Adapter<ListAdapter.ListViewholder>() {
     }
 
     override fun getItemCount(): Int {
-        return 1
+        return listItem.size
     }
 
     override fun onBindViewHolder(holder: ListViewholder, position: Int) {
+        val item = listItem[position]
+
         holder.posicao.text = position.toString()
-        holder.conteudo.text = "lista experimental aula1"
+        holder.conteudo.text = item.city
+
+        holder.root.setOnClickListener{
+            listener.onItemClicked(item.city)
+        }
+
     }
 
     class ListViewholder(binding: LayoutBinding): RecyclerView.ViewHolder(binding.root){
         var posicao = binding.posicao
         var conteudo = binding.conteudo
+        var root = binding.root
 
+    }
+
+    interface ListAdapterListener{
+        fun onItemClicked(content: String);
     }
 }
--- app/src/main/java/com/alexandrestefani/listapp/MainActivity.kt
+++ app/src/main/java/com/alexandrestefani/listapp/MainActivity.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alexandrestefani.listapp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),ListAdapter.ListAdapterListener {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val itemList: MutableList<ListItem> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +23,28 @@ class MainActivity : AppCompatActivity() {
         val listView = binding.lista
         listView.layoutManager = LinearLayoutManager(this)
 
-        listView.adapter = ListAdapter()
+        listView.adapter = ListAdapter(itemList, this)
+
+        createMockData()
 
 
 
     }
+    private fun createMockData(){
+        itemList.add(ListItem("Porto Alegre","RS"))
+        itemList.add(ListItem("Rio de Janeiro","RJ"))
+        itemList.add(ListItem("Sâo Paulo","SP"))
+        itemList.add(ListItem("Brasilia","DF"))
+        itemList.add(ListItem("Florianopolis","SC"))
+        itemList.add(ListItem("Belem","PA"))
+        itemList.add(ListItem("Recife","PE"))
+        itemList.add(ListItem("São Luis","MA"))
+        itemList.add(ListItem("New York","NY"))
+    }
+    override fun onItemClicked(content: String) {
+        Snackbar.make(binding.root, content, Snackbar.LENGTH_SHORT).show()
+
+    }
+
+
 }
